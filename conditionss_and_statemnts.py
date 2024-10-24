4aa3e1300b33c0c39364496e5a0d964802770445
Aswin4442 <aswinbabu4442@gmail.com>
2024-10-24 12:57:50 +0530

    Add files via upload

---
+++ conditionss_and_statemnts.py
@@ -0,0 +1,59 @@
+# if statement
+
+# a=50
+# b=100
+# if b>a:
+    # print("yes,b is greaterthan a")
+
+# if a<b:
+    # print("a is less than b")
+
+
+# elif statement
+
+# x=100
+# y=100
+# if x>y:
+#     print("x is greater than y")
+# elif x==y:
+#     print("x is equal to y")
+
+# a=22
+# b=33
+# if a==b:
+#     print("a equal to b")
+# elif a!=b:
+#     print(" a and b are not eqaul")
+# elif a<b:
+#     print("a is less than b")
+
+
+# else statement
+
+# a=20
+# b=10
+# c=40
+# if a>c:
+#     print("a is greater than c")
+# elif b>a:
+#     print("b is less than a")
+# else:
+#     print("a is less than c")
+
+
+# nested if statement
+
+# x=50
+# y=60
+# if x<=55:
+#     print("below 55")
+# if x>y:
+#     print("50 is less than 60")
+# else:
+#     print("x and y are not equal")
+
+
+
+
+
+
---
+++ taskk_1.py
@@ -0,0 +1,42 @@
+# # a=-10
+# # b=30
+# # if a<0:
+# #     print("a number is negative")
+# # if b>0:
+# #     print("b number is positive")
+# # else:
+# #     print("b number is positive")
+
+
+# find positive or negative
+
+
+
+# a=int(input('enter a number'))
+# if a<0:
+#     print("num is negative")
+# elif a==0:
+#     print('number is zero')
+
+# else:
+#     print("number is positive")
+
+
+# a=int(input('enter your mark'))
+# b=50
+# if a<b:
+#     print("u are failed")
+# elif a==b:
+#     print("u are just pass")
+# else:
+#     print("u are qualified")
+
+
+# 2 even or odd
+
+# a=int(input('enter a number'))
+# if a%2:
+#     print("number is odd")
+# else:
+#     print("nuber is even")
+
