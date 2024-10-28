149c1a60d601e8746363b46d49db6b2ade2f2563
Aswin4442 <aswinbabu4442@gmail.com>
2024-10-28 09:39:08 +0530

    Add files via upload

---
+++ python_loops.py
@@ -0,0 +1,81 @@
+# for loop
+
+# cars=["bmw","benz","audi"]
+# for x in cars:
+#     print(x)
+
+# loop through strng
+# for a in "aeroplane":
+    # print(a)
+
+
+# while loop
+
+# i=1
+# while i<11:
+    # print(i)
+    # i+=1
+
+# control statements
+
+# break
+# i=1
+# while i<10:
+#     print(i)
+#     if(i==5):
+    #  break
+    # i+=1
+
+# # continue 
+# i=0
+# while i<20:
+#     i+=1
+#     if i==10:
+#         continue
+#     print(i)
+#     if(i==15):
+#         break
+
+# else
+# i=1
+# while i<10:
+#     print(i)
+#     i+=1
+# else:
+#     print("mone 10 illa")
+
+# break using in for loop
+
+# cars=["bmw","benz","audi","ninja"]
+# for x in cars:
+#     print(x)
+#     if x=="audi":
+#         break
+
+# colors=["red","black","blue"]
+# for a in colors:
+#     print(a)
+#     if a=="red":
+#         break
+
+# continue in loop
+
+# cars=["bmw","benz","audi"]
+# for i in cars:
+    # if i=="benz":
+        # continue
+    # print(i)
+
+#  range in for loop
+
+# for x in range(2,20,2):
+    # print(x)
+
+# pass statement
+
+# for x in [0,1,2]:
+    # pass
+
+
+
+
