9d49a47195bde28df9ea3ce03ac08f2067c05a25
Aswin4442 <aswinbabu4442@gmail.com>
2024-10-29 13:13:33 +0530

    Add files via upload

---
+++ listss.py
@@ -0,0 +1,38 @@
+# thislist=["apple","banana","mango"]
+# print(thislist)
+
+# list length
+
+# mycars=["bmw","benz","audi"]
+# print(len(mycars))
+
+# diff data type
+
+# list1=["bmw","benz"]
+# list2=[1,2,3]
+# list3=[True,False,True]
+# print(list1)
+# print(list2)
+# print(list3)
+
+# find type
+
+# mylist=["bmw","benz","audi"]
+# print(type(mylist))
+
+# access list item
+
+# mycars=["bmw","benz","audi"]
+# print(mycars[0])
+# print(mycars[1])
+# print(mycars[-1])
+
+# find range of access index
+
+# numbers=[1,2,3,4,5,6,7,8]
+# print(numbers[0:5])
+# print(numbers[-6:-1])
+# print(numbers[4:])
+# print(numbers[:5])
+
+ 
