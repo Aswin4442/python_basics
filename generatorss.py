2400dd7f73c73032dd165824035230659864beaa
Aswin4442 <aswinbabu4442@gmail.com>
2024-10-31 13:09:57 +0530

    Add files via upload

---
+++ generatorss.py
@@ -0,0 +1,21 @@
+# def generator():
+#     yield "aswin"
+#     yield "akash"
+#     yield "karthi"
+# for x in generator():
+#     print(x)
+
+
+# NEXT FUNC
+
+# def number():
+#     yield 1
+#     yield 2
+#     yield 3
+#     yield 4
+# x=number()
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+
---
+++ userdef_func_argumnts.py
@@ -0,0 +1,56 @@
+# FUNCTIONS 
+
+# create func
+
+# def my_function():
+#     print("hello world")
+# my_function()
+
+# ARGUMENTS
+
+# def name(last_name):
+#     print("karthik"+last_name)
+# name(" G")
+
+
+# def multiply_numbrs(a,b):
+#     product=a*b
+#     print("answer is",product)
+# multiply_numbrs(10,45)
+
+
+# DEFAULT ARGUMENTS
+
+# def add_numbrs(a=5,b=8,c=7):
+#     sum=a+b+c
+#     print("answer =",sum)
+# # add_numbrs()
+# add_numbrs(4,3,3)
+# add_numbrs(b=3)
+
+# def div_numbers(x=63,y=7):
+#     ans=x/y
+#     print("answer is",ans)
+# div_numbers()
+# div_numbers(80,8)
+# div_numbers(x=70)
+
+
+# KEYWORD ARGUMENT
+
+# def actor(fname,lname):
+#     print("first name",fname)
+#     print("last name",lname)
+# actor(fname="MOHAN",lname="LAL")
+
+
+# ARBITARY ARGUMENT
+
+# def find_sum(*numbers):
+#     result=0
+#     for x in numbers:
+#         result= result + x
+#     print("answer =",result)
+# find_sum(23,30,67)
+
+
